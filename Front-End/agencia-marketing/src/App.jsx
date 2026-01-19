aa23fa0d1cd6802b44e913bca6db656bae93a71b
TomasMartino <tomasdiamante882@gmail.com>
2026-01-19 20:22:59 -0300

    page product

--- Front-End/agencia-marketing/src/App.jsx
+++ Front-End/agencia-marketing/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import ProductPage from "./Screens/ProductPage";
 import Home from "./Screens/Home";
 /* import Courses from "./pages/Courses";
 import Journal from "./pages/Journal";
@@ -10,8 +10,9 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/product" element={<ProductPage />} />
        {/*  <Route path="/courses" element={<Courses />} />
-        <Route path="/journal" element={<Journal />} />
+        <Route path="/Product" element={<Journal />} />
         <Route path="/contact" element={<Contact />} /> */}
       </Routes>
     </BrowserRouter>
