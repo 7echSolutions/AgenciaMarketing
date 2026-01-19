6cc96aff54b671cfb855b92defbd5e1f01d64489
FernandoDiaz01 <fdiaz4823@gmail.com>
2026-01-19 20:37:00 -0300

    prueba stash

--- Front-End/agencia-marketing/src/App.jsx
+++ Front-End/agencia-marketing/src/App.jsx
@@ -1,5 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 import ProductPage from "./Screens/ProductPage";
+=======
+import Courses from "./Screens/Courses";
+>>>>>>> Stashed changes
+=======
+import Courses from "./Screens/Courses";
+>>>>>>> Stashed changes
 import Home from "./Screens/Home";
 /* import Courses from "./pages/Courses";
 import Journal from "./pages/Journal";
@@ -10,9 +18,18 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
         <Route path="/product" element={<ProductPage />} />
        {/*  <Route path="/courses" element={<Courses />} />
         <Route path="/Product" element={<Journal />} />
+=======
+=======
+>>>>>>> Stashed changes
+        <Route path="/courses" element={<Courses />} />
+       {/*  
+        <Route path="/journal" element={<Journal />} />
+>>>>>>> Stashed changes
         <Route path="/contact" element={<Contact />} /> */}
       </Routes>
     </BrowserRouter>
---
+++ Front-End/agencia-marketing/src/Screens/components/CoursesCard.jsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+
+export default function CoursesCard({ title, icon, desc }) {
+  return (
+    
+      <div className="hover-lift relative flex flex-col justify-between p-8 rounded-lg bg-surface-dark border border-white/5 hover:border-primary/30 transition-all duration-300 h-full min-h-[320px]">
+        
+        <div>
+          <div className="mb-6 w-12 h-12 flex items-center justify-center rounded-full bg-black/30 border border-white/10 group-hover:border-primary/50 transition-colors">
+            <span className="material-symbols-outlined text-primary text-2xl group-hover:scale-110 transition-transform">
+              {icon}
+            </span>
+          </div>
+
+          <h3 className="font-display text-xl font-bold text-white mb-2 group-hover:text-primary transition-colors">
+            {title}
+          </h3>
+
+          <p className="font-body text-sm text-slate-400 leading-relaxed">
+            {desc}
+          </p>
+        </div>
+
+        <div className="pt-8 mt-auto">
+
+           <Link to="/courses" className="group">
+           <span className="flex items-center gap-2 text-xs font-bold uppercase tracking-widest text-white group-hover:text-primary transition-colors">
+            View Details
+            <span className="material-symbols-outlined text-sm transition-transform group-hover:translate-x-1">
+              arrow_forward
+            </span>
+          </span>
+         </Link>
+        </div>
+
+      </div>
+    
+  );
+}
